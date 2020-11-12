579288a5716228b8a53b135f79f989263078bf7b
Romain QUECHON <romain.quechon@gmail.com>
2020-11-12 16:32:42 +0100

    Cocktail routes, model, controller, views done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing database'
+Ingredient.destroy_all
+
+puts 'Creating 3 fake ingredients...'
+ingredient1 = Ingredient.create(name: 'lemon')
+ingredient2 = Ingredient.create(name: 'ice')
+ingredient3 = Ingredient.create(name: 'mint leaves')
+
+puts 'Saving 1st ingredient...'
+ingredient1.save!
+
+puts 'Saving 2nd ingredient...'
+ingredient2.save!
+
+puts 'Saving 3rd ingredient...'
+ingredient3.save!
+
+puts 'Finished!'
